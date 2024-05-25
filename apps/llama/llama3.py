d4b9666ae73e241192df72c6358003a216593539
neoguojing <guojing_neo@163.com>
2024-05-25 21:34:04 +0800

    up

--- apps/llama/llama3.py
+++ apps/llama/llama3.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 from langchain.callbacks.manager import CallbackManagerForLLMRun
+import transformers
 import os
 import sys
 # 获取当前脚本所在的目录路径
@@ -52,17 +53,18 @@ class Llama3(CustomerLLM):
         ))
         self.model_path = model_path
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        # model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+        # model_id = "meta-llama/Meta-Llama-3-8B"
 
         # tokenizer = AutoTokenizer.from_pretrained(model_id)
-        # tokenizer.save_pretrained(os.path.join(model_root,"llama3"))
+        # tokenizer.save_pretrained(os.path.join(model_root,"llama3-base"))
         # model = AutoModelForCausalLM.from_pretrained(
         #     model_id,
         #     torch_dtype=torch.bfloat16,
         #     device_map="auto",
         # )
 
-        # model.save_pretrained(os.path.join(model_root,"llama3"))
+        # model.save_pretrained(os.path.join(model_root,"llama3-base"))
+        
         
        
         self.model.to(self.device)
@@ -90,23 +92,9 @@ class Llama3(CustomerLLM):
     ) -> str:
         if stop is not None:
             self.stop_words_ids.extend([self.tokenizer.encode(stop_) for stop_ in stop])
-        
-        system = kwargs.pop('system', '')
-        history = kwargs.pop('history', [])
-        
-        messages = []
-        messages.append({"role": "system", "content": system})
-        for row in history:
-            if row is None or len(row) < 2:
-                continue
-            messages.append({"role": "user", "content": row[0]})
-            messages.append({"role": "assistant", "content": row[1]})
-        messages.append({"role": "user", "content": prompt})
-        print("Llama3 messages:",messages)
 
-        input_ids = self.tokenizer.apply_chat_template(
-                messages,
-                add_generation_prompt=True,
+        input_ids = self.tokenizer.encode(
+                prompt,
                 return_tensors="pt"
             ).to(self.model.device)
         print("Llama3 stop:",self.react_stop_words_tokens)
@@ -120,7 +108,9 @@ class Llama3(CustomerLLM):
             # stop_words_ids=self.stop_words_ids,
         )
         response = outputs[0][input_ids.shape[-1]:]
+
         return self.tokenizer.decode(response, skip_special_tokens=True)
+        
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
@@ -128,10 +118,15 @@ class Llama3(CustomerLLM):
         return {"model_path": self.model_path}
 
 
-# if __name__ == '__main__':
-#     prompt = '''
-#     俄乌战争
-#     '''
-#     model = Llama3(model_path=os.path.join(model_root,"llama3"))
-#     out = model._call(prompt,system="你是一个政治专家,请使用中文",history=[["二战","不知道"]])
-#     print(out)
+if __name__ == '__main__':
+    prompt = '''
+    俄乌战争
+    '''
+    # model = Llama3(model_path=os.path.join(model_root,"llama3-base"))
+    # out = model._call(prompt,system="你是一个政治专家,请使用中文",history=[["二战","不知道"]])
+    # print(out)
+
+    pipeline = transformers.pipeline(
+            "text-generation", model=os.path.join(model_root,"llama3-base") , model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+        )
+    print(pipeline(prompt))
