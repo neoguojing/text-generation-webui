b2bd86067f419f36f7c3487c4f7c218f0ca25cbb
guoerjun <guoerjun@sensetime.com>
2024-09-12 14:51:03 +0800

    format code

--- apps/model_factory.py
+++ apps/model_factory.py
@@ -9,11 +9,6 @@ top_package_path = os.path.abspath(os.path.join(current_dir, ".."))
 # 将顶层package路径添加到sys.path
 sys.path.insert(0, top_package_path)
 from langchain_community.llms import OpenAI,Tongyi
-from langchain_community.llms import HuggingFacePipeline
-from typing import Any, List, Mapping, Optional
-from langchain.callbacks.manager import CallbackManagerForLLMRun
-from langchain.llms.base import LLM
-from langchain_core.language_models import BaseChatModel
 from apps.translate.nllb import Translate
 from apps.multi_task.speech import SeamlessM4t,Whisper,XTTS
 # from apps.multi_task.speech import SeamlessM4t,Whisper
@@ -24,16 +19,9 @@ from apps.llama.llama3_chat import Llama3Chat
 from apps.embedding.embedding import Embedding
 from apps.config import model_root
 from apps.base import CustomerLLM
-from pydantic import  Field, root_validator
-import torch
 import threading
 import gc
-import weakref
-import pdb
-
-from langchain_core.messages import BaseMessage, HumanMessage,AIMessage,SystemMessage
 from langchain_community.chat_models import QianfanChatEndpoint
-from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult,LLMResult
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # openai
 os.environ['OPENAI_API_KEY'] = ''
@@ -44,68 +32,73 @@ os.environ["QIANFAN_SK"] = "your_sk"
 os.environ["DASHSCOPE_API_KEY"] = ""
 
 class ModelFactory:
-    temperature = 0
     _instances = {}
-    _lock = threading.Lock()  # 异步锁
+    _lock = threading.Lock()
 
     @staticmethod
-    def get_model(model_name,model_path=""):
+    def get_model(model_name, model_path=""):
+        """获取模型实例，并缓存"""
         if model_name not in ModelFactory._instances or ModelFactory._instances[model_name] is None:
             with ModelFactory._lock:
                 if model_name not in ModelFactory._instances or ModelFactory._instances[model_name] is None:
-                    print(f"loading the model {model_name},wait a minute...")
-                    if model_name == "openai":
-                        instance = OpenAI()
-                    elif model_name == "qwen2": 
-                        model_path = os.path.join(model_root,"qwen2")
-                        instance = Qwen2Chat(model_path=model_path)
-                    elif model_name == "qianfan": 
-                        instance = QianfanChatEndpoint(streaming=True, model="ERNIE-Bot-4")
-                    elif model_name == "tongyi": 
-                        instance = Tongyi()
-                    elif model_name == "llama3": 
-                        model_path = os.path.join(model_root,"llama3")
-                        # instance = Llama3(model_path=model_path)
-                        instance = Llama3Chat(model_path=model_path,token=None)
-                    elif model_name == "translate": 
-                        model_path = os.path.join(model_root,"nllb/")
-                        instance = Translate(model_path=model_path)
-                    elif model_name == "speech": 
-                        instance = SeamlessM4t()
-                    elif model_name == "text2image": 
-                        instance = StableDiff()
-                    elif model_name == "image2image": 
-                        instance = Image2Image()
-                    elif model_name == "speech2text": 
-                        instance = Whisper()
-                    elif model_name == "text2speech": 
-                        instance = XTTS()
-                    elif model_name == "embedding": 
-                        instance = Embedding()
-                    else:
-                        raise Exception("Invalid model name")
-                    
-                    ModelFactory._instances[model_name] = instance
-                    print(f"model {model_name} load finished...")
-                    
-        return  ModelFactory._instances[model_name]
-    
+                    ModelFactory._instances[model_name] = ModelFactory._load_model(model_name, model_path)
+        return ModelFactory._instances[model_name]
+
+    @staticmethod
+    def _load_model(model_name, model_path=""):
+        """实际负责模型加载的私有方法"""
+        print(f"Loading the model {model_name}, wait a minute...")
+        if model_name == "openai":
+            return OpenAI()
+        elif model_name == "qwen2": 
+            model_path = os.path.join(model_root,"qwen2")
+            return Qwen2Chat(model_path=model_path)
+        elif model_name == "qianfan": 
+            return QianfanChatEndpoint(streaming=True, model="ERNIE-Bot-4")
+        elif model_name == "tongyi": 
+            return Tongyi()
+        elif model_name == "llama3": 
+            model_path = os.path.join(model_root,"llama3")
+            return Llama3Chat(model_path=model_path,token=None)
+        elif model_name == "translate": 
+            model_path = os.path.join(model_root,"nllb/")
+            return Translate(model_path=model_path)
+        elif model_name == "speech": 
+            return SeamlessM4t()
+        elif model_name == "text2image": 
+            return StableDiff()
+        elif model_name == "image2image": 
+            return Image2Image()
+        elif model_name == "speech2text": 
+            return Whisper()
+        elif model_name == "text2speech": 
+            return  XTTS()
+        elif model_name == "embedding": 
+            return Embedding()
+        else:
+            raise ValueError(f"Invalid model name: {model_name}")
+
     @staticmethod
     def destroy(model_name):
+        """销毁模型实例"""
         if model_name in ModelFactory._instances and ModelFactory._instances[model_name] is not None:
             with ModelFactory._lock:
-                if model_name in ModelFactory._instances and ModelFactory._instances[model_name] is not None:
-                    obj = ModelFactory._instances.get(model_name)
-                    refcount = len(gc.get_referrers(obj))
-                    # print(f"{type(obj)} refer by {refcount} object")
-                    if refcount <= 2:
-                        ModelFactory._instances[model_name] = None
-                        refcount = len(gc.get_referrers(obj))
-                        # print(f"----{type(obj)} refer by {refcount} object")
-                        if isinstance(obj, CustomerLLM) and refcount <= 1 :
-                            obj.destroy()
+                instance = ModelFactory._instances.pop(model_name, None)
+                if instance:
+                    ModelFactory._safe_destroy(instance)
+
+    @staticmethod
+    def _safe_destroy(instance):
+        """安全销毁模型实例"""
+        refcount = len(gc.get_referrers(instance))
+        if refcount <= 2:
+            if isinstance(instance, CustomerLLM):
+                instance.destroy()
+            del instance
+            gc.collect()
 
     @staticmethod
     def release():
-        for k, model in ModelFactory._instances.items():
-            ModelFactory.destroy(k)
+        """释放所有模型实例"""
+        for model_name in list(ModelFactory._instances.keys()):
+            ModelFactory.destroy(model_name)
